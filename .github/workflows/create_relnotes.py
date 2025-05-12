6e0e21c017bf655da3fe826489a4bc331cddf424
zap <134786889+zapSNH@users.noreply.github.com>
2025-05-12 16:32:50 +0800

    better workflow stuff

--- .github/workflows/create_relnotes.py
+++ .github/workflows/create_relnotes.py
@@ -1,25 +1,28 @@
 import sys
+import re
 
-def create_relnotes(ver_v):
-	current_esr = open(".github/workflows/ESR_VERSION", 'r').read()
-	ver = ver_v.replace("v", "")
-	ver_string = "v" + str(int(ver)-1) + "...v" + ver
+def create_relnotes(ver, prev_ver, ext_ver):
+	current_esr = "v" + open(".github/workflows/ESR_VERSION", 'r').read()
+	ver_num_only = re.match(r'^v(\d+)', ver).group(1)
+	ver_num_only_esr = re.match(r'^v(\d+)', current_esr).group(1)
+
 	notes = \
-f'''# zapsCoolPhotonTheme for Firefox {ver} and Firefox {current_esr}esr
-`zapsCoolPhotonTheme-v{ver}.zip` is for Firefox {ver}.
-`zapsCoolPhotonTheme-v{ver}-esr.zip` is for Firefox {current_esr}esr.
+f'''# zapsCoolPhotonTheme for Firefox {ver_num_only} and Firefox {ver_num_only_esr}esr
+`zapsCoolPhotonTheme-{ver}.zip` is for Firefox {ver_num_only}.
+`zapsCoolPhotonTheme-{ver}-esr.zip` is for Firefox {ver_num_only_esr}esr.
 
 ### Changes
 * TBA
 ____
 
-Corresponding Webextension Version: [v0.0.0](https://github.com/zapSNH/zcpt-webextension/releases/tag/v0.0.0)
+Corresponding Webextension Version: [{ext_ver}](https://github.com/zapSNH/zcpt-webextension/releases/tag/{ext_ver})
 
 ____
-**Full Changelog**: https://github.com/zapSNH/zapsCoolPhotonTheme/compare/{ver_string}'''
+**Full Changelog**: https://github.com/zapSNH/zapsCoolPhotonTheme/compare/{prev_ver}...{ver}'''
 	f = open('relnotes.md', 'w')
 	f.write(notes)
 	f.close()
 
 if __name__ == '__main__':
-	globals()[sys.argv[1]](sys.argv[2])
+    a, b, c = sys.argv[1], sys.argv[2], sys.argv[3]
+    create_relnotes(a, b, c)
