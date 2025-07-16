29b061e065e19bbdcf86d0cb7d44bee7d6d34d76
zapbot ratbot! <134786889+zapSNH@users.noreply.github.com>
2025-07-16 15:10:26 +0800

    backport the new workflow for 115

--- .github/workflows/create_relnotes.py
+++ .github/workflows/create_relnotes.py
@@ -1,41 +1,24 @@
-'''# zapsCoolPhotonTheme for Firefox 126 and Firefox 115.11esr
-`zapsCoolPhotonTheme-v126.zip` is for Firefox 126.
-`zapsCoolPhotonTheme-115esr.zip` is for Firefox 115esr.
-
-### Changes
-* Add `uc.always-show-close-button` which always shows the tab close button even when overflowing.(#3)
-* The sidebar should now respect your color theme. (#4)
-* Search bar has now been photonized.
-* Padding adjustements and fixes.
-* Other fixes for Firefox 127.
-____
-
-Corresponding Webextension Version: [v0.0.0](https://github.com/zapSNH/zcpt-webextension/releases/tag/v0.0.0)
-
-____
-**Full Changelog**: https://github.com/zapSNH/zapsCoolPhotonTheme/compare/v125...v126'''
 import sys
+import re
 
-def create_relnotes(ver_v):
-	current_esr = open(".github/workflows/ESR_VERSION", 'r').read()
-	ver = ver_v.replace("v", "")
-	ver_string = "v" + str(int(ver)-1) + "...v" + ver
+def create_relnotes(ver):
+	current_esr = "v" + open(".github/workflows/ESR_VERSION", 'r').read()
+	ver_num_only_esr = re.match(r'^v(\d+)', current_esr).group(1)
+
 	notes = \
-f'''# zapsCoolPhotonTheme for Firefox {ver} and Firefox {current_esr}esr
-`zapsCoolPhotonTheme-v{ver}.zip` is for Firefox {ver}.
-`zapsCoolPhotonTheme-v{ver}-esr.zip` is for Firefox {current_esr}esr.
+f'''# zapsCoolPhotonTheme for Firefox {ver_num_only_esr}esr
+Based on zapsCoolPhotonTheme v{ver_num_only_esr} for Firefox {current_esr}esr.
 
 ### Changes
-* TBA
-____
-
-Corresponding Webextension Version: [v0.0.0](https://github.com/zapSNH/zcpt-webextension/releases/tag/v0.0.0)
+* Removed the aero fog in the titlebar for Windows 7 (#31)
+* The tab highlight is now also aligned in all modes (i think)
 
 ____
-**Full Changelog**: https://github.com/zapSNH/zapsCoolPhotonTheme/compare/{ver_string}'''
+**Full Changelog**: https://github.com/zapSNH/zapsCoolPhotonTheme/compare/v128...{ver}'''
 	f = open('relnotes.md', 'w')
 	f.write(notes)
 	f.close()
 
 if __name__ == '__main__':
-	globals()[sys.argv[1]](sys.argv[2])
+    a, b, c = sys.argv[1], sys.argv[2], sys.argv[3]
+    create_relnotes(a, b, c)
